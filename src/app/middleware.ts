1323df17295e7568972808e43c394b53145ec15c
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-08-23 15:27:39 +0530

    edits

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(req: NextRequest) {
   const adminHeader = req.headers.get("admin");
 
   // Validate the header
-  if (adminHeader !== "sharmahritujeet@gmail.com") {
+  if (adminHeader !== process.env.ADMIN) {
     return new NextResponse(
       JSON.stringify({ error: "Unauthorized access" }),
       { status: 401, headers: { "Content-Type": "application/json" } }
