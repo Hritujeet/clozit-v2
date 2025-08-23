532a0d2c7a0516574432ef995d925f73ecaafcf9
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-08-23 15:39:04 +0530

    fetch-product-by-slug

--- src/app/api/dashboard/v1/products/fetch/[slug]/route.ts
+++ src/app/api/dashboard/v1/products/fetch/[slug]/route.ts
@@ -1,5 +1,20 @@
+import { db } from "@/utils/db/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-    return NextResponse.json("This is fetch a single product")
+export async function GET(
+    request: NextRequest,
+    { params }: { params: Promise<{ slug: string }> }
+) {
+    const { slug } = await params;
+    const product = await db.product.findUnique({
+        where: {
+            slug: slug
+        }
+    })
+
+    if (!product) {
+        throw new Error("Product was not found")
+    }
+
+    return NextResponse.json({message: "This is fetch a single product", data: product})
 }
